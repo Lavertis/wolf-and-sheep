b2a657c1313a8e12f995567d147a07e4d9e465c5
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2023-12-18 18:07:15 +0100

    feat: create xcode project

---
+++ WolfAndSheep/WolfAndSheep/WolfAndSheepApp.swift
@@ -0,0 +1,17 @@
+//
+//  WolfAndSheepApp.swift
+//  WolfAndSheep
+//
+//  Created by Rafał on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WolfAndSheepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
